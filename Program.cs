9f6ebfbebc29b5b6fd3fa332d1eab84edef8f1f0
User <User@DESKTOP-2OOA2TK>
2024-01-24 16:54:01 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Labsheet04
+{
+    static void Main()
+    {
+        Console.Write("Enter the value of N: ");
+
+        string userInput = Console.ReadLine();
+
+        if (int.TryParse(userInput, out int n))
+        {
+            PrintFibonacciSeries(n);
+        }
+        else
+        {
+            Console.WriteLine("ERROR: Please enter a valid integer.");
+        }
+    }
+
+    static void PrintFibonacciSeries(int n)
+    {
+        if (n < 0)
+        {
+            Console.WriteLine("ERROR: Please enter a non-negative integer.");
+            return;
+        }
+        int a = 0, b = 1;
+        Console.Write($"Fibonacci Series (first {n} terms): {a}, {b}");
+
+        for (int i = 2; i < n; i++)
+        {
+            int nextTerm = a + b;
+            Console.Write($", {nextTerm}");
+
+            a = b;
+            b = nextTerm;
+        }
+        Console.WriteLine();
+        Console.ReadLine();
+    }
+}
+
